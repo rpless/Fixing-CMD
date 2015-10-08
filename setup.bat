a59d747315b7fd806481f3cf28c7aa96c850e64a
Ryan Plessner <plessner.ryan@gmail.com>
2015-10-08 15:20:02 -0400

    Update setup.bat with newer NuGet install

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
-@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 if ERRORLEVEL 1 goto :error
 cinst Git.Install -notsilent && cinst ConsoleZ
 if ERRORLEVEL 1 goto :error
